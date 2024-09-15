22e8615c14122f5fd19b7f749867c97b7bb939d4
Sercan Yiğit <sercnyigit@gmail.com>
2024-09-15 17:46:33 +0300

    [sercan.yigit][init project]

---
+++ wear/src/main/java/com/sercan/yigit/wearos/data/transfer/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.sercan.yigit.wearos.data.transfer.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WearOSDataTransferTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
